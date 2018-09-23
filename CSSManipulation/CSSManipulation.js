02d49a3ba1f0a38601225a9e6a25cdf25bb149d3
Tanner Strickler <tstrickler@ku.edu>
2018-09-23 13:06:45 -0500

    add input checking

--- CSSManipulation/CSSManipulation.js
+++ CSSManipulation/CSSManipulation.js
@@ -1,5 +1,4 @@
-function updateCSS()
-{
+function updateCSS() {
   let borderRed = document.getElementById("borderRed").value;
   let borderGreen = document.getElementById("borderGreen").value;
   let borderBlue = document.getElementById("borderBlue").value;
@@ -7,10 +6,18 @@ function updateCSS()
   let backgroundRed = document.getElementById("backgroundRed").value;
   let backgroundGreen = document.getElementById("backgroundGreen").value;
   let backgroundBlue = document.getElementById("backgroundBlue").value;
+    
+  if (borderRed < 0 || borderRed > 255 || borderGreen < 0 || borderGreen > 255 || borderBlue < 0 || borderBlue > 255 || backgroundRed < 0 || backgroundRed > 255 || backgroundGreen < 0 || backgroundGreen > 255 || backgroundBlue < 0 || backgroundBlue > 255) {
+    alert("Color values must be in range 0-255.");
+  }
+  else if (!["thin", "medium", "thick"].includes(borderWidth.toLowerCase())) {
+    alert("Border width must be set to either thin, medium, or thick.");
+  }
+  else {
+    let text = document.getElementById("text");
 
-  let text = document.getElementById("text");
-
-  text.style.borderColor = `rgb(${borderRed}, ${borderGreen}, ${borderBlue})`;
-  text.style.borderWidth = `${borderWidth}`.toLowerCase();
-  text.style.backgroundColor = `rgb(${backgroundRed}, ${backgroundGreen}, ${backgroundBlue})`;
+    text.style.borderColor = `rgb(${borderRed}, ${borderGreen}, ${borderBlue})`;
+    text.style.borderWidth = `${borderWidth}`.toLowerCase();
+    text.style.backgroundColor = `rgb(${backgroundRed}, ${backgroundGreen}, ${backgroundBlue})`;
+  }
 }
